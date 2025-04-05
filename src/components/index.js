94f3ac47add061b4d2088736511f72a99d56a73b
DummyAcc <198493988+dummy69vk@users.noreply.github.com>
2025-04-05 16:02:57 +0530

    Formatting

--- src/components/index.js
+++ src/components/index.js
@@ -9,14 +9,23 @@ import RTE from "./RTE";
 import Select from "./Select";
 import Signup from "./Signup";
 import Login from "./Login";
-import PostForm from './post-form/PostForm'
-import PostCard from './PostCard'
-import AuthLayout from './AuthLayout'
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 
-
-export {Header,Footer,Container,Logo,LogoutBtn,
-    Button,Input,RTE,Select,Signup, Login,
-    PostForm,
-    PostCard,
-    AuthLayout
-}
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  RTE,
+  Select,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
